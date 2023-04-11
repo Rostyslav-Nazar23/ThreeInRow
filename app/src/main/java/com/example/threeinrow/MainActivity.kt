34aec347de3df7f56699c2f54e674314b919100a
Rostyslav Nazarenko <rostislav.nazarenko23@gmail.com>
2023-04-11 21:59:35 +0300

    Beginning of Credit Screen Work

--- app/src/main/java/com/example/threeinrow/MainActivity.kt
+++ app/src/main/java/com/example/threeinrow/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.threeinrow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -47,8 +53,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(navController: NavController) {
-    Column(modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+    ) {
         PlayButton(navController)
         SettingsButton(navController)
         CreditsButton(navController)
@@ -62,7 +72,19 @@ fun SettingsScreen(navController: NavController) {
 
 @Composable
 fun CreditsScreen(navController: NavController) {
-    /*TODO*/
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        flingBehavior = ScrollableDefaults.flingBehavior(),
+        userScrollEnabled = true
+    ) {
+        item { Text("1", fontSize = 30.sp) }
+        item { Text("2", fontSize = 30.sp) }
+        item { Text("3", fontSize = 30.sp) }
+    }
 }
 
 @Composable
